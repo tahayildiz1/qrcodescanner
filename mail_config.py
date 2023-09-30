a87e8b17807a8d333f855cec7d899d25774a747b
Taha <116551153+tahayildiz1@users.noreply.github.com>
2023-10-01 00:39:20 +0200

    Create mail_config.py

---
+++ mail_config.py
@@ -0,0 +1,12 @@
+# mail_config.py
+
+# Email Configuration
+MAIL_CONFIG = {
+    'MAIL_SERVER': ' ',
+    'MAIL_PORT': ,
+    'MAIL_USE_TLS': ,
+    'MAIL_USE_SSL': ,
+    'MAIL_USERNAME': ' ',
+    'MAIL_PASSWORD': ' ',
+    'MAIL_DEFAULT_SENDER': ' '
+}
